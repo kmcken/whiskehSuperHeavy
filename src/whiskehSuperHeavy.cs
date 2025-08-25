5a6ecdc8c524f19f84aacf281f0c5ac6686f2fde
Kirt McKenna <34045021+kmcken@users.noreply.github.com>
2025-08-24 23:53:35 -0600

    init

---
+++ src/whiskehSuperHeavy.cs
@@ -0,0 +1,18 @@
+﻿using UnityGERunner;  // Or AIPSim namespace
+using UnityGERunner.UnityApplication;
+
+public class AIPProvider : IAIPProvider {
+    public override SetupActions Start(SetupInfo info)
+    {
+        return new SetupActions { name = "whiskehXv0" };
+    }
+
+    public override InboundState Update(OutboundState state)
+    {
+        return new InboundState
+        {
+            pyr = new NetVector { x = 0, y = 0, z = 0 },
+            throttle = 100
+        };
+    }
+}
