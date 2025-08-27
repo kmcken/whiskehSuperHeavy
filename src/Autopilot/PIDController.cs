be83131d695f2e835cef0b701fee304affdc2fc2
Kirt McKenna <34045021+kmcken@users.noreply.github.com>
2025-08-26 21:41:04 -0600

    autopilot

---
+++ src/Autopilot/PIDController.cs
@@ -0,0 +1,34 @@
+﻿namespace whiskehSuperHeavy;
+using System;
+
+public class PIDController
+{
+    public float Kp { get; set; }  // Proportional gain
+    public float Ki { get; set; }  // Integral gain
+    public float Kd { get; set; }  // Derivative gain
+
+    private float integral = 0f;
+    private float previousError = 0f;
+
+    public PIDController(float kp, float ki, float kd)
+    {
+        Kp = kp;
+        Ki = ki;
+        Kd = kd;
+    }
+
+    public float Update(float error, float deltaTime)
+    {
+        integral += error * deltaTime;  // Accumulate integral
+        float derivative = (error - previousError) / deltaTime;
+        previousError = error;
+
+        return (Kp * error) + (Ki * integral) + (Kd * derivative);
+    }
+
+    public void Reset()
+    {
+        integral = 0f;
+        previousError = 0f;
+    }
+}
